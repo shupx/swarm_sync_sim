c2461ff63376af2b152497c78ebfb8997338a509
Peixuan Shu <shupeixuan@qq.com>
2024-10-12 19:25:10 +0800

    fix px4 thrust

--- src/px4_rotor_sim/src/mavros_px4_quadrotor_sim/px4_modules/mavlink/mavlink_receiver.cpp
+++ src/px4_rotor_sim/src/mavros_px4_quadrotor_sim/px4_modules/mavlink/mavlink_receiver.cpp
@@ -1535,55 +1535,55 @@ MavlinkReceiver::handle_message_odometry(mavlink_message_t *msg)
 
 void MavlinkReceiver::fill_thrust(float *thrust_body_array, uint8_t vehicle_type, float thrust)
 {
-	// // Fill correct field by checking frametype
-	// // TODO: add as needed
-	// switch (_mavlink->get_system_type()) {
-	// case MAV_TYPE_GENERIC:
-	// 	break;
+	// Fill correct field by checking frametype
+	// TODO: add as needed
+	switch (_mavlink->get_system_type()) {
+	case MAV_TYPE_GENERIC:
+		break;
 
-	// case MAV_TYPE_FIXED_WING:
-	// case MAV_TYPE_GROUND_ROVER:
-	// 	thrust_body_array[0] = thrust;
-	// 	break;
+	case MAV_TYPE_FIXED_WING:
+	case MAV_TYPE_GROUND_ROVER:
+		thrust_body_array[0] = thrust;
+		break;
 
-	// case MAV_TYPE_QUADROTOR:
-	// case MAV_TYPE_HEXAROTOR:
-	// case MAV_TYPE_OCTOROTOR:
-	// case MAV_TYPE_TRICOPTER:
-	// case MAV_TYPE_HELICOPTER:
-	// case MAV_TYPE_COAXIAL:
-	// 	thrust_body_array[2] = -thrust;
-	// 	break;
+	case MAV_TYPE_QUADROTOR:
+	case MAV_TYPE_HEXAROTOR:
+	case MAV_TYPE_OCTOROTOR:
+	case MAV_TYPE_TRICOPTER:
+	case MAV_TYPE_HELICOPTER:
+	case MAV_TYPE_COAXIAL:
+		thrust_body_array[2] = -thrust;
+		break;
 
-	// case MAV_TYPE_SUBMARINE:
-	// 	thrust_body_array[0] = thrust;
-	// 	break;
+	case MAV_TYPE_SUBMARINE:
+		thrust_body_array[0] = thrust;
+		break;
 
-	// case MAV_TYPE_VTOL_TAILSITTER_DUOROTOR:
-	// case MAV_TYPE_VTOL_TAILSITTER_QUADROTOR:
-	// case MAV_TYPE_VTOL_TILTROTOR:
-	// case MAV_TYPE_VTOL_FIXEDROTOR:
-	// case MAV_TYPE_VTOL_TAILSITTER:
-	// case MAV_TYPE_VTOL_RESERVED4:
-	// case MAV_TYPE_VTOL_RESERVED5:
-	// 	switch (vehicle_type) {
-	// 	case vehicle_status_s::VEHICLE_TYPE_FIXED_WING:
-	// 		thrust_body_array[0] = thrust;
+	case MAV_TYPE_VTOL_TAILSITTER_DUOROTOR:
+	case MAV_TYPE_VTOL_TAILSITTER_QUADROTOR:
+	case MAV_TYPE_VTOL_TILTROTOR:
+	case MAV_TYPE_VTOL_FIXEDROTOR:
+	case MAV_TYPE_VTOL_TAILSITTER:
+	case MAV_TYPE_VTOL_RESERVED4:
+	case MAV_TYPE_VTOL_RESERVED5:
+		switch (vehicle_type) {
+		case vehicle_status_s::VEHICLE_TYPE_FIXED_WING:
+			thrust_body_array[0] = thrust;
 
-	// 		break;
+			break;
 
-	// 	case vehicle_status_s::VEHICLE_TYPE_ROTARY_WING:
-	// 		thrust_body_array[2] = -thrust;
+		case vehicle_status_s::VEHICLE_TYPE_ROTARY_WING:
+			thrust_body_array[2] = -thrust;
 
-	// 		break;
+			break;
 
-	// 	default:
-	// 		// This should never happen
-	// 		break;
-	// 	}
+		default:
+			// This should never happen
+			break;
+		}
 
-	// 	break;
-	// }
+		break;
+	}
 }
 
 void
